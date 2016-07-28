189ee19796a84ec70aaf279c1f201f74b4ef4a5e
moon <zhaojucun@yingu.com>
2016-07-28 15:24:33 +0800

    1

---
+++ src/main/java/com/cn/interceptor/HttpRequestInterceptor.java
@@ -0,0 +1,28 @@
+package com.cn.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by moon on 16/7/28.
+ * 拦截器
+ *
+ */
+public class HttpRequestInterceptor implements HandlerInterceptor {
+
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
