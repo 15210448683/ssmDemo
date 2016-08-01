5628dc171ea900f75fb20312f586a222c96c786e
moon <zhaojucun@yingu.com>
2016-08-01 16:53:25 +0800

    调整springmvc注解配置文件

--- src/main/java/com/cn/interceptor/HttpRequestInterceptor.java
+++ src/main/java/com/cn/interceptor/HttpRequestInterceptor.java
@@ -18,8 +18,11 @@ public class HttpRequestInterceptor implements HandlerInterceptor {
      * Controller方法调用之前调用。SpringMVC的这种Interceptor链式结构也是可以进行中断的，这种中断方式是令preHandle的返
      * 回值为false，当preHandle的返回值为false的时候整个请求就结束了。
      */
+    //Action之前执行:
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        System.out.println("1111");
+        //如果返回false则中断请求
+        return true;
     }
     /**
      * 这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。postHandle是进行处理器拦截用的，它的执行时间是在处理器进行处理之
@@ -28,6 +31,7 @@ public class HttpRequestInterceptor implements HandlerInterceptor {
      * 只是Struts2里面的intercept方法中要手动的调用ActionInvocation的invoke方法，Struts2中调用ActionInvocation的invoke方法就是调用下一个Interceptor
      * 或者是调用action，然后要在Interceptor之前调用的内容都写在调用invoke之前，要在Interceptor之后调用的内容都写在调用invoke方法之后。
      */
+    //生成视图之前执行
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
     }
@@ -35,6 +39,7 @@ public class HttpRequestInterceptor implements HandlerInterceptor {
      * 该方法也是需要当前对应的Interceptor的preHandle方法的返回值为true时才会执行。该方法将在整个请求完成之后，也就是DispatcherServlet渲染了视图执行，
      * 这个方法的主要作用是用于清理资源的，当然这个方法也只能在当前这个Interceptor的preHandle方法的返回值为true时才会执行。
      */
+    //最后执行，可用于释放资源
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
     }
